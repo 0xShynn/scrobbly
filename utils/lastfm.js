34228d5c42abb195d681364e9e4bd8f555582b27
Antonin Nhek <a.nhek@icloud.com>
2020-10-22 13:54:03 +0200

    updated: lastfm values.

--- utils/lastfm.js
+++ utils/lastfm.js
@@ -1,5 +1,5 @@
-export const baseUrl = 'http://ws.audioscrobbler.com/2.0/'
-export const api_key = process.env.API_KEY
+export const baseUrl = 'https://ws.audioscrobbler.com/2.0/'
+export const api_key = process.env.LASTFM_API_KEY
 export const username = 'shynnobi'
 
 export const periods = [
