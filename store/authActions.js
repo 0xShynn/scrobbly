c4ae99b4448ff2c7de0fa31377a32b57bd5996bc
Antonin Nhek <a.nhek@icloud.com>
2020-10-23 12:57:17 +0200

    fixed: asyncStorage package.

--- store/authActions.js
+++ store/authActions.js
@@ -1,5 +1,5 @@
 import * as Crypto from 'expo-crypto'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import AsyncStorage from '@react-native-community/async-storage'
 import { baseUrl } from '../utils/lastfm'
 
 export const AUTHENTICATE = 'AUTHENTICATE'
