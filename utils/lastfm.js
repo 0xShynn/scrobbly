a8afb23ba8968d4189d3e653b0a0a2f240185b56
Antonin Nhek <a.nhek@icloud.com>
2020-10-26 13:45:48 +0100

    added: emptyImage url of lastfm.

--- utils/lastfm.js
+++ utils/lastfm.js
@@ -1,6 +1,7 @@
 export const baseUrl = 'https://ws.audioscrobbler.com/2.0/'
 export const api_key = process.env.LASTFM_API_KEY
-export const username = 'shynnobi'
+export const emptyImage =
+  'https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png'
 
 export const periods = [
   { duration: '7day', name: 'Last 7 days' },
