25f06a32e6c7e9d1cf489f8abaca74547421ffe0
“anhek” <a.nhek@icloud.com>
2021-02-09 17:52:52 +0100

    updated: about this app screen styling.

--- screens/AboutThisApp.js
+++ screens/AboutThisApp.js
@@ -1,27 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { Image } from 'react-native';
+import Constants from 'expo-constants';
 import useColorScheme from 'react-native/Libraries/Utilities/useColorScheme';
+
 import CenteredContainer from '../components/UI/CenteredContainer';
 import CustomText from '../components/UI/CustomText';
 import myColors from '../constants/myColors';
+import spacing from '../constants/spacing';
 
 const AboutThisApp = () => {
   const isDarkTheme = useColorScheme() === 'dark' ? true : false;
 
   return (
-    <ScrollView
+    <CenteredContainer
       style={{
+        paddingHorizontal: spacing.lg,
         backgroundColor: isDarkTheme ? myColors.gray_1100 : myColors.gray_100,
-        flex: 1,
       }}
     >
-      <CenteredContainer style={{ marginTop: 30 }}>
-        <CustomText size="H3" bold>
-          Scrobbly
-        </CustomText>
-      </CenteredContainer>
-    </ScrollView>
+      <Image
+        source={require('../assets/icon-rounded.png')}
+        style={{
+          width: 80,
+          height: 80,
+          marginBottom: spacing.lg,
+        }}
+      />
+      <CustomText size="H3" bold complementaryStyle={{ marginBottom: 10 }}>
+        Scrobbly version {Constants.manifest.version}
+      </CustomText>
+
+      <CustomText complementaryStyle={{ marginBottom: 20 }}>
+        Expo version {Constants.expoVersion}
+      </CustomText>
+
+      <CustomText size="H7">
+        The app was created by Antonin Nhek using React Native and Expo.
+        Copyright © {new Date().getFullYear()} Scrobbly.
+      </CustomText>
+    </CenteredContainer>
   );
 };
 
