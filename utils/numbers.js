faa02bf7d4f402733441da9fd5e5c7c8fbf9bc8e
“anhek” <a.nhek@icloud.com>
2021-02-18 18:07:19 +0100

    updated: number.js (eslint)

--- utils/numbers.js
+++ utils/numbers.js
@@ -1,19 +1,22 @@
-const SI_SYMBOL = ['', 'K', 'M', 'G', 'T', 'P', 'E']
+/* eslint-disable no-bitwise */
+const SI_SYMBOL = ["", "K", "M", "G", "T", "P", "E"];
 
-export const abbreviateNumber = (number) => {
+function abbreviateNumber(number) {
   // what tier? (determines SI symbol)
-  let tier = (Math.log10(Math.abs(number)) / 3) | 0
+  const tier = (Math.log10(Math.abs(number)) / 3) | 0;
 
   // if zero, we don't need a suffix
-  if (tier == 0) return number
+  if (tier === 0) return number;
 
   // get suffix and determine scale
-  let suffix = SI_SYMBOL[tier]
-  let scale = Math.pow(10, tier * 3)
+  const suffix = SI_SYMBOL[tier];
+  const scale = 10 ** (tier * 3);
 
   // scale the number
-  let scaled = number / scale
+  const scaled = number / scale;
 
   // format number and add suffix
-  return scaled.toFixed(1) + suffix
+  return scaled.toFixed(1) + suffix;
 }
+
+export default abbreviateNumber;
